83b89a21993750ff3260664710b12c7bfeed85f5
Axel Rosén <axel.rosen@lunicore.se>
2018-03-28 09:44:40 +0200

    add reading of words and printing of children to nodes

--- wordLadder.go
+++ wordLadder.go
@@ -14,10 +14,34 @@ type node struct {
 			children []node
 }
 
+func checkWords(word1 string, word2 string) bool{
+	arr1 := strings.Split(word1,"")
+	arr2 := strings.Split(word2,"")
+
+	counter := 0
+	for i:= 1; i <= 4; i++{
+		for j:= 0; j < len(arr2); j++{
+			if arr2[j] == arr1[i]{
+	            arr1[i] = ""
+	            arr2[j] = ""
+	            counter++
+	            break
+	        }
+	    }
+	}
+	if(counter == 4){
+		return true
+	}else{
+		return false
+	}
+}
+
 func main() {
 		reader := bufio.NewReader(os.Stdin)
 		//fmt.Print("Enter text; ")
 		words := make([]string, 0)
+		children := make(map[string]string, 0)
+
 		for {
 				text, err  := reader.ReadString('\n')
 				if err == io.EOF {
@@ -26,24 +50,30 @@ func main() {
 				words = append(words, text)
 		}
 		fmt.Println(words)
-
+		nodes := make([]node, 0)
 		for i := 0; i < len(words); i++ {
 				currNode := node{words[i], make([]node, 0)}
 				for j:=0; j < len(words); j++ {
 					if j == i {
 									continue
 					} else {
-							isMatching := strings.ContainsRune(words[j],rune(words[i][1])) &&
-														strings.ContainsRune(words[j],rune(words[i][2])) &&
-														strings.ContainsRune(words[j],rune(words[i][3])) &&
-														strings.ContainsRune(words[j],rune(words[i][4]))
-
+							isMatching := checkWords(words[i], words[j])
 							if isMatching {
 									newNode := node{words[j], make([]node, 0)}
 									currNode.children = append(currNode.children, newNode)
+									children[currNode.name] += newNode.name
 							}
 					}
 				}
+				nodes = append(nodes, currNode)
+		}
+		for _, element := range nodes {
+			
+			fmt.Println(element.name, ": ")
+			for _, ele2 := range element.children {
+				fmt.Print(ele2.name)
+			}
+			fmt.Println("__________")
 		}
 }
 
