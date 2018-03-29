6ca56d67370f98beee7d7fc3a57229a4a61fced3
Axel Rosén <axel.rosen@lunicore.se>
2018-03-29 14:55:04 +0200

    Almost working solution

--- closestPair.go
+++ closestPair.go
@@ -12,8 +12,8 @@ import (
 )
 
 type Item struct {
-		value []int
-		priority int
+		value []float64
+		priority float64
 		index int
 }
 
@@ -43,13 +43,13 @@ func (pq *PriorityQueue) Pop() interface{} {
 	old := *pq
 	n := len(old)
 	item := old[n-1]
-	//item.index = -1 // for safety
+	// item.index = -1 // for safety
 	*pq = old[0 : n-1]
 	return item
 }
 
 // update modifies the priority and value of an Item in the queue.
-func (pq *PriorityQueue) update(item *Item, value []int, priority int) {
+func (pq *PriorityQueue) update(item *Item, value []float64, priority float64) {
 	item.value = value
 	item.priority = priority
 	heap.Fix(pq, item.index)
@@ -79,20 +79,20 @@ func main() {
 	_ , _ = reader.ReadString('\n')
 	for {
 				text, err  := reader.ReadString('\n')
-				if err == io.EOF || strings.EqualFold(text, "EOF\n") {
+				if err == io.EOF {
 					break
 				}
 				myNums := strings.Split(text, " ")
 				myNums[1] = strings.TrimSuffix(myNums[1], "\n")
-				x, _:= strconv.Atoi(myNums[0])
-				y, _:= strconv.Atoi(myNums[1])
+				x, _:= strconv.ParseFloat(myNums[0], 64)
+				y, _:= strconv.ParseFloat(myNums[1], 64)
 				pointx := &Item{
-										value: []int{x,y},
+										value: []float64{x,y},
 										priority: x,
 										index: i,
 				}
 				pointy := &Item{
-							value: []int{x,y},
+							value: []float64{x,y},
 							priority: y,
 							index: i,
 				}
@@ -100,6 +100,9 @@ func main() {
 				heap.Push(&py, pointy)
 				i++
 		}
+		// for _, element := range px{
+		// 	fmt.Println(*element)
+		// }
 		//for px.Len() > 0 {
 		//	item := heap.Pop(&px).(*Item)
 		//	fmt.Print("Index: ", item.index, " ")
@@ -110,6 +113,7 @@ func main() {
 		//}
 		//fmt.Println(px)
 		firstHalf := px[0:(len(px)/2)]
+
 		secondHalf := px[(len(px)/2):]
 		firstHalfy := py[0:(len(py)/2)]
 		secondHalfy := py[(len(py)/2):]
@@ -119,7 +123,6 @@ func main() {
 		var rx float64
 		var ry float64
 		var wg sync.WaitGroup
-
 		go findClosest(firstHalf, &qx, 1, &wg)
 		wg.Add(1)
 		go findClosest(secondHalf, &rx, 2, &wg)
@@ -133,45 +136,63 @@ func main() {
 		//fmt.Println("qx: ", qx, " rx: ", rx)
 		//fmt.Println("qy: ", qy, " ry: ", ry)
 		//fmt.Println(delta)
-		if delta == math.Inf(1) {
-						delta = 10000
-		}
-		xstar := firstHalf[len(firstHalf)-1]
+		// if delta == math.Inf(1) {
+		// 				delta = 10000
+		// }
+		// fmt.Println(delta)
+		// for _, element := range secondHalf{
+		// 	fmt.Println(element)
+		// }
+		xstar := heap.Pop(&firstHalf).(*Item)
 		sy := make(PriorityQueue, 0)
 		heap.Init(&sy)
-		for _ , element := range py {
-				if (element.value[0] - xstar.value[0]) < (int(delta)+1) {
+		// fmt.Println(xstar)
+		for py.Len() > 0 {
+				element := heap.Pop(&py).(*Item)
+				if math.Abs(element.value[0] - xstar.value[0]) < delta {
 								heap.Push(&sy, element)
 				}
 		}
+		// fmt.Println(sy.Len())
+		// for _, element := range sy{
+		// 	fmt.Print(element.value[0], " ")
+		// 	fmt.Println(element.value[1])
+		// }
 		//fmt.Println(sy)
 		//fmt.Println(xstar)
 		min := math.Inf(1)
 		//myPoints := make([]int, 2)
 		length := len(sy)
-		var innerLoop int
-		if (length - 1) < 15 {
-			innerLoop = length - 1
-		} else {
-			innerLoop = 15
-		}
+		// var innerLoop int
+		// if (length - 1) < 1000 {
+		// 	innerLoop = length
+		// } else {
+		// 	innerLoop = 1000
+		// }
 		for index, point := range sy {
-				for i:=index +1; i <= innerLoop; i++ {
+				for i:=index +1; i < length; i++ {
+					if math.Abs(point.value[1] - sy[i].value[1]) > delta{
+						break
+					}
 						currDist := distance(point.value, sy[i].value)
 						if min > currDist{
 			 			 		min = currDist
+			 			 		// fmt.Println(point.value , " ", sy[i].value)
 			 			 		//myPoints = []int{index, index+1}
 			 			}
 				}
 		}
-		//fmt.Println(min)
+		// for _, element := range sy{
+		// 	fmt.Println(*element)
+		// }
+		// fmt.Println(min)
 		trueMin := math.Min(min, delta)
 		if trueMin == math.Inf(1) {
 				trueMin = 0
 		}
-		fmt.Printf("%.15f\n",trueMin)
-}
-func distance(p1 []int, p2 []int) float64 {
+		fmt.Printf("%.6f\n",trueMin)
+}	
+func distance(p1 []float64, p2 []float64) float64 {
 	  var dist float64
 		deltax := float64(p1[0] - p2[0])
 		deltay := float64(p1[1] - p2[1])
@@ -180,22 +201,28 @@ func distance(p1 []int, p2 []int) float64 {
 }
 func findClosest(q PriorityQueue, retVal *float64, goNo int, wg *sync.WaitGroup) {
 		min := math.Inf(1)
-		//myPoints := make([]int, 2)
-		length := len(q)
-		//fmt.Println("length ", length, "IN GOROUTINE: ", goNo)
-		for index, point := range q {
-			 //fmt.Println("INDEX ", index, "IN GOROUTINE: ", goNo)
-			 if index > length-2{
-					break
-			 }
-			 currDist := distance(point.value, q[index + 1].value)
-			 if min > currDist{
-						min = currDist
-						//myPoints = []int{index, index+1}
-			 }
-		}
-		//fmt.Println("GO:", goNo, " ",myPoints)
-		//fmt.Println(min)
-		*retVal = min
-		wg.Done()
+        //myPoints := make([]int, 2)
+        // length := q.Len()
+        //fmt.Println("length ", length, "IN GOROUTINE: ", goNo)
+        prevElement := heap.Pop(&q).(*Item)
+        for q.Len() > 0 {
+            currElement := heap.Pop(&q).(*Item)
+             //fmt.Println("INDEX ", index, "IN GOROUTINE: ", goNo)
+             if currElement == nil{
+                    break
+             }
+             currDist := distance(prevElement.value, currElement.value)
+             if min > currDist{
+                if goNo == 2{
+                // fmt.Println(prevElement.value, " ", currElement.value)
+                }
+                        min = currDist
+                        //myPoints = []int{index, index+1}
+             }
+             prevElement = currElement
+        }
+        //fmt.Println("GO:", goNo, " ",myPoints)
+        //fmt.Println(min)
+        *retVal = min
+        wg.Done()
 }
